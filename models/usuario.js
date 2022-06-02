aafb29cb58756a2daf8e2c6cf23cd6fbfaf724c0
William Juarez <wgbila@gmail.com>
2022-06-02 14:59:48 -0600

    se quito el atributo user

--- models/usuario.js
+++ models/usuario.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  user: String,
   password: String,
   nombre: String,
   correo: String,
@@ -16,7 +15,7 @@ schema.methods.generateAuthToken = function () {
     {
       _id: this._id,
       rol: this.rol,
-      user: this.user,
+      correo: this.correo,
       nombre: this.nombre,
     },
     "ruinas_teotihuacan"
