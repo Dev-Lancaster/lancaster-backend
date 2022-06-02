2f793c72b564de50d4881edb1f3294ac64a944c8
William Juarez <wgbila@gmail.com>
2022-06-02 12:46:26 -0600

    db url

--- startup/db_url.js
+++ startup/db_url.js
@@ -1,9 +1,11 @@
 const config = require("config");
 
-const MONGO_URL = `mongodb+srv://${config.get("database.user")}:${config.get(
+/*const MONGO_URL = `mongodb+srv://${config.get("database.user")}:${config.get(
   "database.password"
 )}@cluster0.kc8kbjs.mongodb.net/${config.get(
   "database.name"
-)}?retryWrites=true&w=majority`;
+)}?retryWrites=true&w=majority`;*/
+
+const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
 
 exports.MONGO_URL = MONGO_URL;
