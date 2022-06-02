6c43d2641fbecc8280ebf627624705adf0b5cce6
William Juarez <wgbila@gmail.com>
2022-06-02 14:13:20 -0600

    remove config

--- startup/db_url.js
+++ startup/db_url.js
@@ -1,11 +1,10 @@
-const config = require("config");
-
 /*const MONGO_URL = `mongodb+srv://${config.get("database.user")}:${config.get(
   "database.password"
 )}@cluster0.kc8kbjs.mongodb.net/${config.get(
   "database.name"
 )}?retryWrites=true&w=majority`;*/
 
-const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
+//const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb://wjuarez:Ju4r3zRu4n0DB001@192.168.1.59:27017/COMERC_DESA`;
 
 exports.MONGO_URL = MONGO_URL;
