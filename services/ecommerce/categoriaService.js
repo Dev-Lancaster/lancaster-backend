3c258263d2aa012ca154a1cbbf8bbe47c28b99f8
William Juarez <wgbila@gmail.com>
2022-06-03 09:22:52 -0600

    creación de servicio de categorias

---
+++ services/ecommerce/categoriaService.js
@@ -0,0 +1,50 @@
+const { Categoria } = require("../../models/categoria");
+
+async function findAll() {
+  return await Categoria.find().sort({ nombre: 1 }).lean();
+}
+
+async function findActive() {
+  return await Categoria.find({ activo: true }).sort({ nombre: 1 }).lean();
+}
+
+async function findById(id) {
+  return await Categoria.findById(id).lean();
+}
+
+async function save(model) {
+  if (!model)
+    return { type: "EMPTY", message: "Los datos de la categoría estan vacios" };
+  if (!model.nombre)
+    return { type: "EMPTY", message: "El nombre de la categoría esta vacio" };
+  if (!model.usuarioCrea)
+    return {
+      type: "EMPTY",
+      message: "El nombre del usuario que crea la categoría esta vacio",
+    };
+  let categoria = new Categoria(model);
+  await categoria.save();
+  return categoria;
+}
+
+async function inactivate(id) {
+  const model = await Categoria.findByIdAndUpdate(id, { activo: false });
+  return model;
+}
+
+async function activate(id) {
+  const model = await Categoria.findByIdAndUpdate(id, { activo: true });
+  return model;
+}
+
+async function update(id, model) {
+  return await Categoria.findByIdAndUpdate(id, model);
+}
+
+exports.findAll = findAll;
+exports.findActive = findActive;
+exports.findById = findById;
+exports.save = save;
+exports.inactivate = inactivate;
+exports.update = update;
+exports.activate = activate;
