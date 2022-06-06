bf4b61f1942901d4f4dd5883fa00d11d62dfb3f2
Bila <wgbila@gmail.com>
2022-06-05 20:29:36 -0600

    creacion de nuevas variables

--- middleware/constants.js
+++ middleware/constants.js
@@ -1,5 +1,9 @@
 const NOT_VALID = "NOT-VALID";
 const ATR_MISSED = "ATR_MISSED";
+const EMPTY = "EMPTY";
+const SUCCESS = "SUCCESS";
 
 exports.NOT_VALID = NOT_VALID;
 exports.ATR_MISSED = ATR_MISSED;
+exports.EMPTY = EMPTY;
+exports.SUCCESS = SUCCESS;
