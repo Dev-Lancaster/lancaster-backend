6d006c1b4a995ba2054d8c0d95c21612e99335f2
Bila <wgbila@gmail.com>
2022-06-05 19:50:04 -0600

    creacion de endpoint categoria

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const error = require("../middleware/error");
 const auth = require("../api/common/auth");
+const categoria = require("../api/ecommerce/categoria");
 
 module.exports = function (app) {
+  app.use("/categoria", categoria);
   app.use("/auth", auth);
   app.use(error);
 };
