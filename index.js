0e3d9cfbb3810cd2c25a48be50fa1dbc926d51e4
Bila <wgbila@gmail.com>
2022-06-22 19:17:49 -0600

    se quito la config de swagger

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const app = require("./server");
-const swaggerUi = require("swagger-ui-express");
 const product = require("./api/product");
-const swaggerDocument = require("./swagger.json");
 
 require("./startup/db")();
 app.use("/api/product", product);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 1984;
 const server = app.listen(PORT);
