df30cd4661c52a8be676c2639bf80064d12fd152
Bila <wgbila@gmail.com>
2022-06-26 16:23:42 -0600

    save fotos

--- services/ecommerce/productoService.js
+++ services/ecommerce/productoService.js
@@ -2,23 +2,60 @@ const constants = require("../../middleware/constants");
 const fs = require("fs");
 const { Producto } = require("../../models/producto");
 const { FotoProducto } = require("../../models/fotoProducto");
+const _ = require("lodash");
 
-function validateFilename(filename) {}
+const posiciones = ["FRO", "TRA", "IZQ", "DER", "ARR", "ABA"];
+
+async function validateFilename(filename) {
+  if (!filename) return { type: "ERROR", msg: "El archivo viene vacio" };
+  if (typeof filename !== "string")
+    return {
+      type: "ERROR",
+      msg: "El formato del nombre del archivo es incorrecto",
+    };
+  if (!filename.includes("_"))
+    return { type: "ERROR", msg: "El formato del archivo es incorrecto" };
+  if (!filename.split("_").length !== 3)
+    return { type: "ERROR", msg: "El formato del archivo es incorrecto" };
+
+  const array = filename.split("_");
+  const producto = await findProductoByCodigo(array[0]);
+  if (!producto)
+    return { type: "ERROR", msg: "El codigo del producto es invalido" };
+
+  if (_.isNaN(array[1]))
+    return {
+      type: "ERROR",
+      msg: "El valor relacionado al orden de la imagen debe ser un numero",
+    };
+
+  if (!posiciones.includes(array[2]))
+    return {
+      type: "ERROR",
+      msg: "El valor de posicion de la imagen es incorrecto",
+    };
+
+  return { type: "SUCCESS", array: array };
+}
 
 async function saveFoto(producto, files, usuario) {
+  let errors = [];
+  let resultValidate;
   for (const file of files) {
-    let model = new FotoProducto();
-    model.producto = producto;
-    /*
-  posicion: String,
-  orden: Number,
-*/
-    model.usuarioCrea = usuario;
-    model.img.data = fs.readFileSync(file.path);
-    model.img.contentType = file.mimetype;
-    model.fechaCrea = new Date();
-    await model.save();
+    resultValidate = await validateFilename(file.name);
+    if (resultValidate.type === "SUCCESS") {
+      let model = new FotoProducto();
+      model.producto = producto;
+      model.posicion = resultValidate.array[2];
+      model.orden = resultValidate.array[1];
+      model.usuarioCrea = usuario;
+      model.img.data = fs.readFileSync(file.path);
+      model.img.contentType = file.mimetype;
+      model.fechaCrea = new Date();
+      await model.save();
+    } else errors.push(resultValidate);
   }
+  return errors;
 }
 
 async function deleteFoto(id) {
@@ -37,6 +74,17 @@ async function findFotoById(id) {
 async function findFotos(idProducto) {
   const result = await FotoProducto.find({ producto: idProducto })
     .sort({ orden: 1 })
+    .lean();
+  return result;
+}
+
+async function findProductoByCodigo(codigo) {
+  return await Producto.findOne({ codigo: codigo }).lean();
+}
+
+async function findByEtiquetaEstado(etiqueta, estado) {
+  const result = await Producto.find({ estado: estado, etiqueta: etiqueta })
+    .sort({ categoria: 1, nombre: 1 })
     .lean();
   return result;
 }
@@ -82,6 +130,16 @@ function validateProducto(model) {
     return {
       type: constants.ATR_MISSED,
       message: "La talla del producto no puede estar vacio",
+    };
+  if (!model.etiqueta)
+    return {
+      type: constants.ATR_MISSED,
+      message: "Debe definir al menos una etiqueta para el producto",
+    };
+  if (model.etiqueta.length === 0)
+    return {
+      type: constants.ATR_MISSED,
+      message: "Debe definir al menos una etiqueta para el producto",
     };
   if (!model.color)
     return {
@@ -142,3 +200,5 @@ exports.findFotoById = findFotoById;
 exports.deleteFotos = deleteFotos;
 exports.deleteFoto = deleteFoto;
 exports.saveFoto = saveFoto;
+exports.findByEtiquetaEstado = findByEtiquetaEstado;
+exports.findProductoByCodigo = findProductoByCodigo;
