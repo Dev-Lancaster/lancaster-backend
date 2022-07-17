f0dba88543f055c3225df5fa007d120db4ae01f8
Bila <wgbila@gmail.com>
2022-07-17 15:31:49 -0600

    remove api product

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const app = require("./server");
-const product = require("./api/product");
 
 require("./startup/db")();
-app.use("/api/product", product);
 
 const PORT = process.env.PORT || 1984;
 const server = app.listen(PORT);
