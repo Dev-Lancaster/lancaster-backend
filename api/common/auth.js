8b517ee0531cfaf1eb499484680f6014a71a7489
Bila <wgbila@gmail.com>
2022-07-18 20:58:46 -0600

    login

--- api/common/auth.js
+++ api/common/auth.js
@@ -4,8 +4,8 @@ const UsuarioService = require("../../services/admin/UsuarioService");
 
 router.post("/", async (req, res) => {
   const { correo, password } = req.body;
+  //password: "Prueba1#",
   const result = await UsuarioService.validateCredentials(correo, password);
-
   if (result.type === "SUCCESS") res.send(result);
   else res.send(result);
 });
