33262dd2b796ec3b116b84ce195d736dee315b4a
Bila <wgbila@gmail.com>
2022-07-18 20:40:08 -0600

    return result

--- api/common/auth.js
+++ api/common/auth.js
@@ -7,7 +7,7 @@ router.post("/", async (req, res) => {
   const result = await UsuarioService.validateCredentials(correo, password);
 
   if (result.type === "SUCCESS") res.send(result);
-  else res.status(204).send(result);
+  else res.send(result);
 });
 
 module.exports = router;
