e479ee7810fccbe2e2d67f5fd2932d0d501423e2
William Juarez <wgbila@gmail.com>
2022-09-06 13:59:31 -0600

    router

--- startup/routes.js
+++ startup/routes.js
@@ -3,8 +3,10 @@ const auth = require("../api/common/auth");
 const categoria = require("../api/ecommerce/categoria");
 const producto = require("../api/ecommerce/producto");
 const fotos = require("../api/ecommerce/fotos");
+const talla = require("../api/admin/talla");
 
 module.exports = function (app) {
+  app.use("/talla", talla);
   app.use("/fotos", fotos);
   app.use("/producto", producto);
   app.use("/categoria", categoria);
