c5f6947b3fb00ea397c2fef8360d2ad8c323b60b
Bila <wgbila@gmail.com>
2022-09-10 14:41:02 -0600

    padre

--- models/categoria.js
+++ models/categoria.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const schema = new mongoose.Schema({
   nombre: String,
   codigo: String,
   activo: Boolean,
   nivel: Number,
+  padre: { type: Schema.Types.ObjectId, ref: "categoria" },
   usuarioCrea: String,
   fechaCrea: Date,
 });
