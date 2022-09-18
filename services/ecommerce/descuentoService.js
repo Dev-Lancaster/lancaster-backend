98ec7ee70baa0b9a41e31f991577bf0d179d2124
Bila <wgbila@gmail.com>
2022-09-18 16:12:09 -0600

    save

---
+++ services/ecommerce/descuentoService.js
@@ -0,0 +1,34 @@
+const { Descuento } = require("../../models/descuento");
+const { Producto } = require("../../models/producto");
+
+async function maxCodigo() {
+  const descuento = await Descuento.findOne().sort({ codigo: -1 }).lean();
+  if (!descuento) return 1;
+  return descuento.codigo + 1;
+}
+
+async function save(list) {
+  const codigo = await maxCodigo();
+  const fechaCrea = new Date();
+  let model;
+
+  for (let body of list) {
+    body.codigo = codigo;
+    body.fechaCrea = fechaCrea;
+    model = new Descuento(body);
+    await model.save();
+    await Producto.findByIdAndUpdate(body.producto, {
+      precioDescuento: body.valorNuevo,
+      descuento: body.porcentaje,
+    });
+  }
+}
+
+async function findAll() {
+  const list = await Descuento.find().sort({ codigo: -1 }).lean();
+  return list;
+}
+
+exports.maxCodigo = maxCodigo;
+exports.save = save;
+exports.findAll = findAll;
