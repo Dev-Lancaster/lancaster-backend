c9bf1611022094ee8a5a7cac4a30cef6ab04ea99
Bila <wgbila@gmail.com>
2022-09-18 16:17:17 -0600

    router descuento

--- startup/routes.js
+++ startup/routes.js
@@ -4,8 +4,10 @@ const categoria = require("../api/ecommerce/categoria");
 const producto = require("../api/ecommerce/producto");
 const fotos = require("../api/ecommerce/fotos");
 const talla = require("../api/admin/talla");
+const descuento = require("../api/ecommerce/descuento");
 
 module.exports = function (app) {
+  app.use("/descuento", descuento);
   app.use("/talla", talla);
   app.use("/fotos", fotos);
   app.use("/producto", producto);
