bfee9dab082df220a70ff194a7ba4defb0e7114d
Bila <wgbila@gmail.com>
2022-09-18 11:23:11 -0600

    back to origin

--- models/producto.js
+++ models/producto.js
@@ -34,6 +34,6 @@ const schema = new Schema({
   fechaOcupado: Date,
 });
 
-const Producto = mongoose.model("productos_temp", schema);
+const Producto = mongoose.model("productos", schema);
 
 exports.Producto = Producto;
