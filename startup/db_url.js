0b9bfeff9f1837e53462ccac0d41aea9da3d7a03
Bila <wgbila@gmail.com>
2022-10-01 17:59:42 -0600

    ...

--- startup/db_url.js
+++ startup/db_url.js
@@ -1,9 +1,4 @@
-/*const MONGO_URL = `mongodb+srv://${config.get("database.user")}:${config.get(
-  "database.password"
-)}@cluster0.kc8kbjs.mongodb.net/${config.get(
-  "database.name"
-)}?retryWrites=true&w=majority`;*/
-
+//const MONGO_URL = `mongodb+srv://wjuarez:wjuarez23@cluster0.tveqv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&ssl=true`;
 const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
 //const MONGO_URL = `mongodb://wjuarez:Ju4r3zRu4n0DB001@192.168.1.59:27017/COMERC_DESA`;
 
