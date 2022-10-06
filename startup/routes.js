4c1f0de9431750abeb0760e2e06cf7095482d12d
William Juarez <wgbila@gmail.com>
2022-10-06 10:38:20 -0600

    orden compra

--- startup/routes.js
+++ startup/routes.js
@@ -5,8 +5,10 @@ const producto = require("../api/ecommerce/producto");
 const fotos = require("../api/ecommerce/fotos");
 const talla = require("../api/admin/talla");
 const descuento = require("../api/ecommerce/descuento");
+const ordenCompra = require("../api/ecommerce/ordenCompra");
 
 module.exports = function (app) {
+  app.use("/orden", ordenCompra);
   app.use("/descuento", descuento);
   app.use("/talla", talla);
   app.use("/fotos", fotos);
