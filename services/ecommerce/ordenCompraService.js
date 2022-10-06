7f9222a21baa108ea4da812842098b457c14857a
William Juarez <wgbila@gmail.com>
2022-10-06 08:59:41 -0600

    interface

---
+++ services/ecommerce/ordenCompraService.js
@@ -0,0 +1,11 @@
+const { OrdenCompra } = require("../../models/ordenCompra");
+const { Producto } = require("../../models/producto");
+
+async function save(model) {}
+
+async function update(id, model) {}
+
+async function generateCodigo() {}
+
+exports.save = save;
+exports.update = update;
