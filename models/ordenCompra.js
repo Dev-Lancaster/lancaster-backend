0b656817b10518ed7bfbed2039b26b358a8a2f89
Bila <wgbila@gmail.com>
2022-10-09 15:14:24 -0600

    object

--- models/ordenCompra.js
+++ models/ordenCompra.js
@@ -28,6 +28,7 @@ const schema = new mongoose.Schema({
   codigoFact: String,
   status: String,
   totalAmount: Number,
+  nubeFact: Object,
   products: [productoSchema],
   customerDetails: clienteSchema,
   date: Date,
