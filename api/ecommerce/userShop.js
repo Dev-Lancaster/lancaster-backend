b3ae77dea1cf62109520a517d29cb3165403f08b
Bila <wgbila@gmail.com>
2022-10-12 19:33:53 -0600

    api usershop

---
+++ api/ecommerce/userShop.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const UserShopService = require("../../services/ecommerce/userShopService");
+
+router.get("/", async (req, res) => {
+  const result = await UserShopService.findAll();
+  res.send(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await UserShopService.findById(id);
+  res.send(result);
+});
+
+router.post("/", async (req, res) => {
+  const body = req.body;
+  const result = await UserShopService.save(body);
+  res.send(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const result = await UserShopService.update(id, body);
+  res.send(result);
+});
+
+module.exports = router;
