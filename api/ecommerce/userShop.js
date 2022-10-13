87ee41f51bb504cb4d4cb7dd435e23349c49f8ff
Bila <wgbila@gmail.com>
2022-10-12 19:38:42 -0600

    /orden

--- api/ecommerce/userShop.js
+++ api/ecommerce/userShop.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const UserShopService = require("../../services/ecommerce/userShopService");
 
+router.get("/orden/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await UserShopService.getOrdenCompra(id);
+  res.send(result);
+});
+
 router.get("/", async (req, res) => {
   const result = await UserShopService.findAll();
   res.send(result);
