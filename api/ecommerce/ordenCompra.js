44dae9efb336995fec37d63710cb2dcda66d3bc1
William Juarez <wgbila@gmail.com>
2022-10-18 07:06:37 -0600

    /nubefact

--- api/ecommerce/ordenCompra.js
+++ api/ecommerce/ordenCompra.js
@@ -2,6 +2,21 @@ const express = require("express");
 const router = express.Router();
 const OrdenCompraService = require("../../services/ecommerce/ordenCompraService");
 
+router.post("/nubefact", async (req, res) => {
+  const body = req.body;
+  try {
+    const result = await OrdenCompraService.prepareFactura(body);
+    res.send({ type: "SUCCESS", factura: result });
+  } catch (e) {
+    console.error(e);
+    res.send({
+      type: "ERROR",
+      factura: null,
+      message: "Ha ocurrido un error interno",
+    });
+  }
+});
+
 router.post("/ordenado", async (req, res) => {
   const body = req.body;
   try {
