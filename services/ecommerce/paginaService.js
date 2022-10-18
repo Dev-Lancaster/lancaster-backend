c5304a08b9665c1b545f10a5ffd2a242911c17ba
William Juarez <wgbila@gmail.com>
2022-10-18 10:01:18 -0600

    paginaService detail

--- services/ecommerce/paginaService.js
+++ services/ecommerce/paginaService.js
@@ -1,3 +1,66 @@
+const s3 = require("../../middleware/s3");
 const { Pagina } = require("../../models/pagina");
 
-async function save(body) {}
+async function save(body) {
+  const fotos = await saveFotos(body.fotos);
+  body.fotos = fotos;
+
+  const exist = await Pagina.findOne({ tema: body.tema });
+  if (exist) return await update(exist._id, body);
+
+  body.fechaCrea = new Date();
+  let model = new Pagina(body);
+  model = await model.save();
+  return model;
+}
+
+async function findAll() {
+  const result = await Pagina.find().lean();
+  let data = [],
+    fotos = [];
+
+  for (const model of result) {
+    fotos = await getFotos(model.fotos);
+    data.push({
+      tema: model.tema,
+      descripcion: model.descripcion,
+      fotos: fotos,
+      usuarioCrea: model.usuarioCrea,
+      fechaCrea: model.fechaCrea,
+    });
+  }
+
+  return data;
+}
+
+async function getFotos(fotos) {
+  let newFotos = [];
+  let url;
+  for (const model of fotos) {
+    url = await s3.getFileURL(model);
+    newFotos.push(url);
+  }
+  return newFotos;
+}
+
+async function update(id, body) {
+  await Pagina.findByIdAndUpdate(id, body);
+  return await Pagina.findById(id).lean();
+}
+
+async function saveFotos(fotos) {
+  let newFotos = [];
+  let url;
+
+  for (const foto of fotos) {
+    await s3.uploadFile(foto);
+    url = await s3.getFileURL(foto.originalname);
+    newFotos.push(foto.originalname);
+  }
+
+  return newFotos;
+}
+
+exports.save = save;
+exports.findAll = findAll;
+exports.update = update;
