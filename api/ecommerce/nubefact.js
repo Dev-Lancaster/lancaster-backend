3f00eafb86d534d4e13f41e3e563d79bfd270aa6
Bila <dev@lancaster.com.pe>
2022-12-06 20:56:52 -0600

    validacion error nubefact

--- api/ecommerce/nubefact.js
+++ api/ecommerce/nubefact.js
@@ -25,14 +25,14 @@ router.post("/:id", async (req, res) => {
 
     const result = await OrdenCompraService.prepareFactura(body);
     if (result && result.errors && result.codigo && result.codigo === 21) {
-      await OrdenCompraService.errorNubefact(idOrden, body);
-      return {
+      await OrdenCompraService.errorNubefact(idOrden, result);
+      res.send({
         type: "ERROR",
         factura: result,
         message: "Ha ocurrido un error interno",
-      };
+      });
     }
-
+    console.log("5");
     res.send({ type: "SUCCESS", factura: result });
   } catch (e) {
     console.error(e);
