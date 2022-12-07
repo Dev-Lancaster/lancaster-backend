752f172031642dbba879a3f3cd0254046ed94bea
Bila <dev@lancaster.com.pe>
2022-12-06 19:43:00 -0600

    desa/prod

--- startup/db_url.js
+++ startup/db_url.js
@@ -1,5 +1,6 @@
-//const MONGO_URL = `mongodb+srv://wjuarez:wjuarez23@cluster0.tveqv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&ssl=true`;
+//DESA
 const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
-//const MONGO_URL = `mongodb://wjuarez:Ju4r3zRu4n0DB001@192.168.1.59:27017/COMERC_DESA`;
+//PRODUCCION
+//const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.dt9vkjb.mongodb.net/test`;
 
 exports.MONGO_URL = MONGO_URL;
