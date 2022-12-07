f54637b3f4a6c08c6807b966e4b7ca9e1e4692b7
Bila <dev@lancaster.com.pe>
2022-12-06 20:42:45 -0600

    quitar comentarios

--- api/ecommerce/nubefact.js
+++ api/ecommerce/nubefact.js
@@ -2,19 +2,6 @@ const express = require("express");
 const router = express.Router();
 const OrdenCompraService = require("../../services/ecommerce/ordenCompraService");
 
-/**
- *    agregar campo de error en la orden
- *    recuperar el id en la url
- *    recuperar la orden de compra
- *    guardar el body que manda mauri en la orden de compra
- *    verificar si el nubefact dio error o no
- *    si si, poner un atributo en la orden que diga de ese error y mandar correo a valeria
- *    si da error mandar todo bien
- *    si todo sale bien mandar todo bien
- *    en la api de facturado verificar si existe ese atributo para cambiar o no su estado a facturado
- * en la consulta de orden mostrar las que tuvieron error y cual es
- */
-
 router.post("/:id", async (req, res) => {
   const body = req.body;
   const idOrden = req.params.id;
