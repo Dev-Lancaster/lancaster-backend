95c93c9f52370f6b0e03302bf0def790d0206c27
Bila <dev@lancaster.com.pe>
2022-12-11 20:30:43 -0600

    resumen

--- api/ecommerce/ordenExcel.js
+++ api/ecommerce/ordenExcel.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ordenExcelService = require("../../services/ecommerce/ordenExcelService");
 
-router.get("/month/:month/year/:year", async (req, res) => {
+router.get("/resumen/month/:month/year/:year", async (req, res) => {
   const { month, year } = req.params;
   const result = await ordenExcelService.generateResumen(month, year);
   res.send(result);
