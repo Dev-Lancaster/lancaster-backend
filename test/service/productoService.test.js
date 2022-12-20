c5071e63db554a4c30fbbcca32e48299509f6e1f
Bila <dev@lancaster.com.pe>
2022-12-19 20:13:50 -0600

    new

---
+++ test/service/productoService.test.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const productoService = require("../../services/ecommerce/productoService");
+const { MONGO_URL } = require("../../startup/db_url");
+
+describe("Prueba de Producto Service", () => {
+  jest.setTimeout(100000000);
+  beforeAll(async () => {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it("updateExcelInfo", async () => {
+    await productoService.updateExcelInfo("/Users/wjuarez/Desktop/damas.xlsx");
+    expect(true).toBe(true);
+  });
+});
