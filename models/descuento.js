bb5e937a009ffc620b7015fdf7fbcc582ab5abca
Bila <dev@lancaster.com.pe>
2022-12-28 17:28:25 -0600

    descuento producto

--- models/descuento.js
+++ models/descuento.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
   codigo: Number,
   estado: String,
-  producto: { type: Schema.Types.ObjectId, ref: "productos" },
+  producto: { type: Schema.Types.ObjectId, ref: "producto" },
   categoriaPadre: { type: Schema.Types.ObjectId, ref: "categoria" },
   categoriaHija: { type: Schema.Types.ObjectId, ref: "categoria" },
   etiqueta: String,
