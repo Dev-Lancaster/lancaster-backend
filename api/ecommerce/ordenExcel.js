1968f911904deb5a1df7b1852a4cf171ec200d2d
Bila <dev@lancaster.com.pe>
2022-12-28 21:11:15 -0600

    generateExtracto

--- api/ecommerce/ordenExcel.js
+++ api/ecommerce/ordenExcel.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const ordenExcelService = require("../../services/ecommerce/ordenExcelService");
 
+router.get("/extracto/month/:month/year/:year", async (req, res) => {
+  const { month, year } = req.params;
+  const result = await ordenExcelService.generateExtracto(month, year);
+  res.send(result);
+});
+
 router.get("/resumen/month/:month/year/:year", async (req, res) => {
   const { month, year } = req.params;
   const result = await ordenExcelService.generateResumen(month, year);
