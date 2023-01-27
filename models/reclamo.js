2ddc901ed5b471169892dc07570264d27f9b0e1a
Bila <dev@lancaster.com.pe>
2023-01-26 18:33:06 -0600

    new model

---
+++ models/reclamo.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  tipoIdentificacion: String,
+  apellidoPaterno: String,
+  apellidoMaterno: String,
+  identificacion: String,
+  departamento: String,
+  menor: String,
+  apoderado: String,
+  razonSocial: String,
+  firstname: String,
+  phone: String,
+  address: String,
+  country: String,
+  email: String,
+  ruc: String,
+  tipoReclamo: String,
+  monto: Number,
+  descripcion: String,
+  disconformidad: String,
+  detalle: String,
+  pedido: String,
+});
+
+const Reclamo = mongoose.model("reclamo", schema);
+
+exports.Reclamo = Reclamo;
