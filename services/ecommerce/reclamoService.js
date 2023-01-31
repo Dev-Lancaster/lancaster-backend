5ce2db56dab78d82001d79906a6b80b2581324a0
Bila <dev@lancaster.com.pe>
2023-01-30 19:02:35 -0600

    send email

--- services/ecommerce/reclamoService.js
+++ services/ecommerce/reclamoService.js
@@ -1,4 +1,5 @@
 const mail = require("../../middleware/mail");
+const moment = require("moment");
 const general = require("../common/general");
 const { Reclamo } = require("../../models/reclamo");
 
@@ -16,9 +17,47 @@ async function findByEmail(email) {
 }
 
 async function sendEmail(reclamo) {
-  const textHeader = "Se ha generado un reclamo con la siguiente informacion:";
-  const html = general.mailTemplate(textHeader);
-  await mail.sendMail("wgbila@gmail.com", "Generacion de Reclamo", html);
+  if (reclamo) {
+    const textHeader = `
+          <div class="row" style='margin-top: 30px'>
+            <div class="col s12">Se ha generado un nuevo reclamo con la siguiente informaci&oacute;n:</div>
+          </div>
+          <div class="row" style='margin-top: 25px'>
+            <div class="col s12">
+              <table>
+                <tr>
+                  <td><strong>Nombre:</strong></td>
+                  <td>${reclamo.firstname} ${reclamo.apellidoPaterno}</td>
+                </tr>
+                <tr>
+                  <td><strong>Tel&eacute;fono:</strong></td>
+                  <td>${reclamo.phone}</td>
+                </tr>
+                <tr>
+                  <td><strong>Tipo de Reclamo:</strong></td>
+                  <td>${reclamo.tipoReclamo}</td>
+                </tr>
+                <tr>
+                  <td><strong>Descripci&oacute;n:</strong></td>
+                  <td>${reclamo.descripcion}</td>
+                </tr>
+                <tr>
+                  <td><strong>Disconformidad:</strong></td>
+                  <td>${reclamo.disconformidad}</td>
+                </tr>
+                <tr>
+                  <td><strong>Fecha de creaci&oacute;n:</strong></td>
+                  <td>${
+                    reclamo.fechaCrea &&
+                    moment(reclamo.fechaCrea).format("DD/MM/YYYY")
+                  }</td>
+                </tr>
+              </table>
+            </div>
+          </div>`;
+    const html = await general.mailTemplate(textHeader);
+    await mail.sendMail("wgbila@gmail.com", "Generacion de Reclamo", html);
+  }
 }
 
 exports.save = save;
