0d5aba5b23be0ca63afd0dd6b97608debdc78518
Bila <dev@lancaster.com.pe>
2023-03-21 18:20:35 -0600

    guardar error

--- api/ecommerce/nubefact.js
+++ api/ecommerce/nubefact.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const errorService = require("../../services/admin/ErrorService");
 const OrdenCompraService = require("../../services/ecommerce/ordenCompraService");
 
 router.post("/:id", async (req, res) => {
@@ -7,25 +8,30 @@ router.post("/:id", async (req, res) => {
   const idOrden = req.params.id;
   try {
     const ordenCompra = await OrdenCompraService.findById(idOrden);
-    if (!ordenCompra)
-      return {
+    if (!ordenCompra) {
+      await errorService.save(`/nubefact/${req.params.id} - Linea 12`, idOrden);
+      res.send({
         type: "ERROR",
         factura: null,
         message: "No se pudo encontrar la orden de compra",
-      };
+      });
+    }
 
     const flagBody = await OrdenCompraService.updateNubefactBody(idOrden, body);
-    if (!flagBody)
-      return {
+    if (!flagBody) {
+      await errorService.save(`/nubefact/${req.params.id} - Linea 22`, idOrden);
+      res.send({
         type: "ERROR",
         factura: null,
         message:
           "No se pudo actualizar la orden con los datos que se desean comprar",
-      };
+      });
+    }
 
     const result = await OrdenCompraService.prepareFactura(body);
     if (result && result.errors && result.codigo && result.codigo === 21) {
       await OrdenCompraService.errorNubefact(idOrden, result);
+      await errorService.save(`/nubefact/${req.params.id} - Linea 34`, idOrden);
       res.send({
         type: "ERROR",
         factura: result,
@@ -35,6 +41,7 @@ router.post("/:id", async (req, res) => {
     res.send({ type: "SUCCESS", factura: result });
   } catch (e) {
     console.error(e);
+    await errorService.save(`/nubefact/${req.params.id}`, e.message);
     res.send({
       type: "ERROR",
       factura: null,
