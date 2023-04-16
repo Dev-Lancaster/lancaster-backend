ad937925c66558f87bf030f66f6decbdcb30ac07
Bila <dev@lancaster.com.pe>
2023-04-16 08:41:27 -0600

    database config

--- startup/db_url.js
+++ startup/db_url.js
@@ -1,6 +1,5 @@
-//DESA
-const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.kc8kbjs.mongodb.net/HORUS_DESA?retryWrites=true&w=majority`;
-//PRODUCCION
-//const MONGO_URL = `mongodb+srv://isacaster:CasterIsa89@cluster0.dt9vkjb.mongodb.net/test`;
+const config = require("config");
+
+const MONGO_URL = `mongodb+srv://${config.database.user}:${config.database.password}@cluster0.kc8kbjs.mongodb.net/${config.database.name}?retryWrites=true&w=majority`;
 
 exports.MONGO_URL = MONGO_URL;
