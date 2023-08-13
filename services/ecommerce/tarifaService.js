fb34ca145119ee939bb05116c5b7cc9e570935e6
Bila <dev@lancaster.com.pe>
2023-08-13 14:18:30 -0600

    find tarifas

--- services/ecommerce/tarifaService.js
+++ services/ecommerce/tarifaService.js
@@ -1,14 +1,39 @@
 const { Tarifas } = require("../../models/tarifas");
+const _ = require("lodash");
 
 async function findTarifas() {
+  const result = await groupByDepartamento();
+
+  const data = result.map((model) => {
+    const provincias = _.groupBy(model.data, "provincia");
+    const formattedProvincias = Object.entries(provincias).map(
+      ([nombre, data]) => ({
+        nombre,
+        data,
+      })
+    );
+    return {
+      departamento: model._id.departamento,
+      provincias: formattedProvincias,
+    };
+  });
+
+  return data;
+}
+
+async function groupByDepartamento() {
   const result = await Tarifas.aggregate([
     {
       $group: {
         _id: {
           departamento: "$departamento",
-          provincia: "$provincia",
         },
         data: { $push: "$$ROOT" },
+      },
+    },
+    {
+      $sort: {
+        "_id.departamento": 1,
       },
     },
   ]);
@@ -16,3 +41,4 @@ async function findTarifas() {
 }
 
 exports.findTarifas = findTarifas;
+exports.groupByDepartamento = groupByDepartamento;
