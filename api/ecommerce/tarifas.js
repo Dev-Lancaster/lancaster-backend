d4d522c8c57ac5a0aa64e4dbcbcbf13b4e81b6f9
Bila <dev@lancaster.com.pe>
2023-08-14 15:26:43 -0600

    crud

--- api/ecommerce/tarifas.js
+++ api/ecommerce/tarifas.js
@@ -7,4 +7,25 @@ router.get("/find", async (req, res) => {
   res.send(result);
 });
 
+router.get("/", async (req, res) => {
+  const result = await tarifaService.findAll();
+  res.send(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const result = await tarifaService.findById(req.params.id);
+  res.send(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const body = req.body;
+  try {
+    await tarifaService.update(req.params.id, body);
+  } catch (e) {
+    console.error(e);
+    res.send({ type: "ERROR" });
+  }
+  res.send({ type: "SUCCESS" });
+});
+
 module.exports = router;
