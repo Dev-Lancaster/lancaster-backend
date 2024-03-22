5604b6b5a5b92ada0b2fef7e464e92aca48402cb
William Juarez <wgbila@gmail.com>
2024-03-21 20:20:00 -0600

    cache

--- api/ecommerce/pagina.js
+++ api/ecommerce/pagina.js
@@ -1,85 +1,180 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const NodeCache = require("node-cache");
 const paginaService = require("../../services/ecommerce/paginaService");
+
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 12000 });
 
 const upload = multer({
   dest: "./upload",
 });
 
 router.get("/titulo/home/i", async (req, res) => {
-  const result = await paginaService.findOneByTema("Tema Principal I");
-  res.send(result);
+  const cacheKey = "/titulo/home/i";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Tema Principal I");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/titulo/home/ii", async (req, res) => {
-  const result = await paginaService.findOneByTema("Tema Principal II");
-  res.send(result);
+  const cacheKey = "/titulo/home/ii";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Tema Principal II");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/contactanos/correo", async (req, res) => {
-  const result = await paginaService.findOneByTema("Contáctanos: correo");
-  res.send(result);
+  const cacheKey = "/contactanos/correo";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Contáctanos: correo");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/contactanos/telefono", async (req, res) => {
-  const result = await paginaService.findOneByTema("Contáctanos: teléfono");
-  res.send(result);
+  const cacheKey = "/contactanos/telefono";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Contáctanos: teléfono");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/contactanos/direccion", async (req, res) => {
-  const result = await paginaService.findOneByTema("Contáctanos: Dirección");
-  res.send(result);
+  const cacheKey = "/contactanos/direccion";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Contáctanos: Dirección");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/pie/pagina", async (req, res) => {
-  const result = await paginaService.findOneByTema("Pie de pagina");
-  res.send(result);
+  const cacheKey = "/pie/pagina";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Pie de pagina");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/nuestra/historia", async (req, res) => {
-  const result = await paginaService.findOneByTema("Nuestra historia");
-  res.send(result);
+  const cacheKey = "/nuestra/historia";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Nuestra historia");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/parallax", async (req, res) => {
-  const result = await paginaService.findOneByTema("Parallax");
-  res.send(result);
+  const cacheKey = "/parallax";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Parallax");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/compromiso/social", async (req, res) => {
-  const result = await paginaService.findOneByTema("Compromiso social");
-  res.send(result);
+  const cacheKey = "/compromiso/social";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema("Compromiso social");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/acerca/segunda", async (req, res) => {
-  const result = await paginaService.findOneByTema("Acerca de segunda sección");
-  res.send(result);
+  const cacheKey = "/acerca/segunda";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findOneByTema(
+      "Acerca de segunda sección"
+    );
+    cache.set(cache, result);
+    res.send(result);
+  }
 });
 
 router.get("/empleados", async (req, res) => {
-  const result = await paginaService.findByTema("Empleados");
-  res.send(result);
+  const cacheKey = "/empleados";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findByTema("Empleados");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/acerca/encabezado", async (req, res) => {
-  const result = await paginaService.findByTema("Acerca de encabezado");
-  res.send(result);
+  const cacheKey = "/acerca/encabezado";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findByTema("Acerca de encabezado");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/collection/type", async (req, res) => {
-  const result = await paginaService.findByTema("Collection Type");
-  res.send(result);
+  const cacheKey = "/collection/type";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findByTema("Collection Type");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/collection", async (req, res) => {
-  const result = await paginaService.findByTema("Collection");
-  res.send(result);
+  const cacheKey = "collection";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findByTema("Collection");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/slider", async (req, res) => {
-  const result = await paginaService.findByTema("Slider");
-  res.send(result);
+  const cacheKey = "/slider";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) res.send(cachedData);
+  else {
+    const result = await paginaService.findByTema("Slider");
+    cache.set(cacheKey, result);
+    res.send(result);
+  }
 });
 
 router.get("/:id", async (req, res) => {
